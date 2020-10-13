bf8537127a7abd5ec72b336d43cee46c0c54703a
ad220 <ad220@users.noreply.github.com>
2020-10-14 00:08:00 +0200

    updated model (v4)

--- discrete_model.py
+++ discrete_model.py
@@ -2,104 +2,146 @@
 import matplotlib.pyplot as plt
 from math import *
 
-def initier_population(n,r=1000):
-    """Crée une population de n individus, de direction donnée (à passer enn programmation orientée objet)"""
-    pop=[[rd.randint(0,r) for i in range(n)],[rd.randint(0,r) for i in range(n)]] #Position de chaque point
-    x,y=[],[]
-    for i in range(n):
-        direction=rd.random()*2*pi #Direction aléatoire
-        x+=[cos(direction)] #Direction selon x
-        y+=[sin(direction)] #Direction selon y
-    return pop+[x,y,[r],[False for i in range(n)]] #On ajoute la taille de l'espace de simulation et l'indicateur pour savoir si un individu est contaminé ou pas
-
-def contaminer_population(P,n=1):
-    """Contamine n éléments dans la population P"""
-    pool=[-1] #Initialise la liste des éléments déjà contaminés pour pas contaminer deux fois la même personne
-    npop=len(P[0])-1
-    for i in range(n):
-        random=-1 #un élément déjà dans pool pour entrer dans la boucle while
-        while random in pool: #on vérifie que l'élément choisi n'a pas déjà été contaminé
-            random=rd.randint(0,npop) #élément aléatoire
-        pool+=[random]
-        P[5][random]=True #contamination
+class population(object):
+    def __init__(self,n,r):
+        """Crée une population de n individus, de direction donnée, dans un espace carré de coté r"""
+        pop=[[rd.randint(0,r) for i in range(n)],[rd.randint(0,r) for i in range(n)]] #Position de chaque point
+        vx,vy=[],[]
+        for i in range(n):
+            direction=rd.random()*2*pi #Direction aléatoire
+            vx+=[cos(direction)] #Direction selon x
+            vy+=[sin(direction)] #Direction selon y
+        self.init = pop+[vx,vy,[r],[False for i in range(n)]]
+        self.n = n
+        self.r = r
+        self.x = pop[0]
+        self.y = pop[1]
+        self.vx = vx
+        self.vy = vy
+        self.coroned = self.init[5]
 
 
-
-def afficher_population(P):
-    """Ne fonctionne plus"""
-    X,Y=[],[]
-    for p in P:
-        x,y,=p
-        X+=[x]
-        Y+=[y]
-    plt.plot(X,Y,'.')
-    plt.show()
+    def __repr__(self):
+        """représente la position de tous les éléments de la population sur un graph"""
+        plt.plot(self.x,self.y,'.')
+        plt.xlim(0,self.r)
+        plt.ylim(0,self.r)
+        plt.show()
 
 
-def propagation(P,m=1,e=1):
-    """Fait se déplacer chaque élément d'un déplacement m et vérifie si deux éléments sont susceptible de se contaminer leur distance relative < e"""
-    b=P[4][0] #On récupère la taille de l'espace de déplacement
-    P[0]=[P[0][i]+m*P[2][i] for i in range(len(P[0]))] #Chaque point se déplace de m selon sa trajectoire (sur x)
-    P[1]=[P[1][i]+m*P[3][i] for i in range(len(P[1]))] # ... de même sur y
-    for i in range(len(P[0])): #On vérifie que les éléments sont toujours dans l'espace d'étude sinon ils "rebondissent"
-        if P[0][i] > b :
-            P[0][i]=b
-            P[2][i]=-P[2][i]
-        elif P[0][i] < 0 :
-            P[0][i]=0
-            P[2][i]=-P[2][i]
+    def contaminer(self,n=1):
+        """Contamine n éléments dans la population"""
+        pool=[-1] #Initialise la liste des éléments déjà contaminés pour pas contaminer deux fois la même personne
+        for i in range(n):
+            random=-1 #un élément déjà dans pool pour entrer dans la boucle while
+            while random in pool: #on vérifie que l'élément choisi n'a pas déjà été contaminé
+                random=rd.randint(0,self.n) #élément aléatoire
+            pool+=[random]
+            self.coroned[random]=True #contamination
 
-        if P[1][i] > b :
-            P[1][i]=b
-            P[3][i]=-P[3][i]
-        elif P[1][i] < 0 :
-            P[1][i]=0
-            P[3][i]=-P[3][i]
 
-    for i in range(len(P[0])-1): #on vérifie si les éléments sont trop proches deux à deux (complexité en n!)
-        for j in range(i+1,len(P[0])):
-            if ((P[0][i]-P[0][j])**2 +(P[1][i]-P[1][j])**2)**(1/2)<e:
-                if P[5][i] or P[5][j]:
-                    P[5][i],P[5][j]=True,True
-    
+    def propagation(self,m=1,e=1):
+        """Fait se déplacer chaque élément d'un déplacement m et vérifie si deux éléments sont susceptible de se contaminer leur distance relative < e"""
+        b=self.r
+        X,Y,vX,vY=self.x,self.y,self.vx,self.vy
+        X=[X[i]+m*vX[i] for i in range(self.n)] #Chaque point se déplace de m selon sa trajectoire (sur x)
+        Y=[Y[i]+m*vY[i] for i in range(self.n)] # ... de même sur y
+
+        for i in range(self.n): #On vérifie que les éléments sont toujours dans l'espace d'étude sinon ils "rebondissent"
+            if X[i] > b :       X[i] , vX[i]  =  b , -vX[i]
+            elif X[i] < 0 :     X[i] , vX[i]  =  0 , -vX[i]
+            if Y[i] > b :       Y[i] , vY[i]  =  b , -vY[i]
+            elif Y[i] < 0 :     Y[i] , vY[i]  =  0 , -vY[i]
+
+        for i in range(self.n-1): #on vérifie si les éléments sont trop proches deux à deux (complexité en n!)
+            for j in range(i+1,self.n):
+                if ((X[i]-X[j])**2 +(Y[i]-Y[j])**2)**(1/2)<e:
+                    if self.coroned[i] or self.coroned[j]:
+                        self.coroned[i],self.coroned[j]=True,True
+        self.x,self.y=X,Y
     
 
+    def reset(self):
+        """réinitialise la population à partir de la liste init"""
+        self.x=self.init[0]
+        self.x=self.init[1]
+        self.vx=self.init[2]
+        self.vx=self.init[3]
+        self.coroned=[False for i in range(self.n)]
 
 
-
-
-
-
-
-def animer(P,m=0.15,frames=1000,fr=0.001): #animation (calcul entre deux affichages) qui fonctionne pour une petite population
-    anim,=plt.plot(P[0],P[1],'.')
-    plt.xlim(0,P[4][0])
-    plt.ylim(0,P[4][0])
+def animer(P,frames=1000,fr=1/60,m=0.15,e=1): 
+    """animation (calcul entre deux affichages) qui fonctionne pour une petite population"""
+    anim,=plt.plot(P.x,P.y,'.')
+    plt.xlim(0,P.r)
+    plt.ylim(0,P.r)
     for i in range(frames):
-        propagation(P,m)
-        anim.set_xdata(P[0])
-        anim.set_ydata(P[1])
+        P.propagation(m,e)
+        anim.set_xdata(P.x)
+        anim.set_ydata(P.x)
         plt.pause(fr)
     plt.show()
 
-def animer_pre(P,frames=1000,fr=0.01,m=0.15,e=1): #animation d'une population (fait tous les calculs avant d'animer pour avoir un animation plus fluide sur une grande population)
-    anim,=plt.plot(P[0],P[1],'.') #crée la figure
-    plt.xlim(0,P[4][0]) #place les limite du graph
-    plt.ylim(0,P[4][0])
-    X,Y=[],[]
-    for i in range(frames):
-        propagation(P,m,e) #fait chaque calcul de propagation
-        X+=[[x for x in P[0]]] #récupère la position de chaque point entre chaque déplacement
-        Y+=[[y for y in P[1]]]
+def animer_pre(P,frames=1000,fr=1/60,m=0.15,e=1):
+    """animation d'une population (fait tous les calculs avant d'animer pour avoir un animation plus fluide sur une grande population)"""
+    anim,=plt.plot(P.x,P.y,'.') #crée la figure
+    plt.xlim(0,P.r) #place les limite du graph
+    plt.ylim(0,P.r)
+    X,Y,C=simulation(P,frames,m,e)
     for i in range(frames):
         anim.set_xdata(X[i]) #ajoute les données instant après instant dans l'animation
         anim.set_ydata(Y[i])
         plt.pause(fr)  #définit les la fréquence d'affichage en déterminant la période de pause entre deux graphiques.
     plt.show()
 
+def animation_coroned(P,duree=1000,fr=1/60,pas=0.15,e=1):
+    """évolution des personnes contaminées"""
+    anim,=plt.plot([],[])
+    plt.xlim(0,duree)
+    plt.ylim(0,P.n)
+    X,Y,C=simulation(P,duree,pas,e)
+    for i in range(duree):
+        anim.set_xdata([j for j in range(i)])
+        anim.set_ydata([sum(C[j]) for j in range(i)])
+        plt.pause(fr)
+    plt.show()
+
+
+def animergold(P,frames=1000,fr=1/60,m=0.15,e=1): 
+    """animation (calcul entre deux affichages) qui fonctionne pour une petite population"""
+    plt.subplot(1,2,1)
+    anim_move,=plt.plot(P.x,P.y,'.')
+    plt.xlim(0,P.r)
+    plt.ylim(0,P.r)
+    X,Y,C=simulation(P,frames,m,e)
+
+    plt.subplot(1,2,2)
+    anim_graph,=plt.plot([],[]) #crée la figure
+    plt.xlim(0,frames)
+    plt.ylim(0,P.n)
+
+    for i in range(frames):
+        anim_move.set_xdata(X[i])
+        anim_move.set_ydata(Y[i])
+        anim_graph.set_xdata([j for j in range(i)])
+        anim_graph.set_ydata([sum(C[j]) for j in range(i)])
+        plt.pause(fr)
+    plt.show()
+
+
+def simulation(P,duree,pas,e):
+    """fait la simulation de la population P sur une certaine duree"""
+    X,Y,C=[],[],[]
+    for i in range(duree):
+        P.propagation(pas,e) #fait chaque calcul de propagation
+        X+=[[x for x in P.x]] #récupère la position de chaque point entre chaque déplacement
+        Y+=[[y for y in P.y]]
+        C+=[[b for b in P.coroned]]
+    return X,Y,C
+
 #faire plusieurs anim de différentes couleurs indépendantes entre elles.
 
-P=initier_population(100,50)
-contaminer_population(P)
-animer_pre(P,100)
+P=population(100,50)
+P.contaminer()
+animergold(P,1000,0.001,0.1)
 breakpoint()
